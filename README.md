621a203f1f13ca49f5e51caf7c39a9249d6540b4
kishore-18vk <mkishore81924@gamil.com>
2025-12-05 23:43:06 +0530

    Add/update .gitignore before cleaning repo

--- README.md
+++
@@ -1,1 +0,0 @@
-# simpleHr
--- employeee/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import get_employees, create_employee, update_employee, delete_employee
-
-urlpatterns = [
-    path('get/', get_employees, name='get-employees'),
-    path('create/', create_employee, name='create-employee'),
-    path('update/', update_employee, name='update-employee'),
-    path('delete/', delete_employee, name='delete-employee'),
-]
